8135fd301bcd2a4312bf47a44ecc8aa31f8be953
Robin Alwert <Ralwert@outlook.com>
2023-11-02 09:14:36 +0100

    Initial Commit

---
+++ pia12v1.2/pia12v1_2App.swift
@@ -0,0 +1,17 @@
+//
+//  pia12v1_2App.swift
+//  pia12v1.2
+//
+//  Created by Robin Alwert on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct pia12v1_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
